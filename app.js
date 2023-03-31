383ee19048c305da33974dd115f3d5da8f45fd0b
rr37 <ay870527@gmail.com>
2023-03-31 10:59:55 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const methodOverride = require('method-override')
 // 引用 body-parser
 const bodyParser = require('body-parser')
 
+// 引用路由器
+const routes = require('./routes')
+
 // 僅在非正式環境時，使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -43,64 +46,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
 
-// 資料庫連線設定
-
-// 設定首頁路由
-app.get('/', (req, res) => {
-  Todo.find() // 取出 Todo model 裡的所有資料
-    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 Javascript 資料陣列
-    .sort({ _id: 'desc' }) // 新增這裡：根據 ＿id 升冪排序
-    .then(todos => res.render('index', { todos })) // 將資料傳給 index 模板
-    .catch(error => console.error(error)) // 錯誤處理
-})
-
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
-
-app.post('/todos', (req, res) => {
-  const name = req.body.name // 從 req.body 拿出表單裡的 name 資料
-  return Todo.create({ name }) // 存入資料庫
-    .then(() => res.redirect('/')) // 新增完成後導回首頁
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/:id', (req,res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('detail', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('edit', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.put('/todos/:id', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
-
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+// 將 request 導入路由器
+app.use(routes)
 
 // 設定 port 3000
 app.listen(3000, () => {
---
+++ routes/modules/home.js
@@ -0,0 +1,15 @@
+// 引用 Express 與 Express 路由器
+const express = require('express')
+const router = express.Router()
+// 引用 Todo model
+const Todo = require('../../models/todo')
+// 定義首頁路由
+router.get('/', (req, res) => {
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 Javascript 資料陣列
+    .sort({ _id: 'desc' }) // 新增這裡：根據 ＿id 升冪排序
+    .then(todos => res.render('index', { todos })) // 將資料傳給 index 模板
+    .catch(error => console.error(error)) // 錯誤處理
+})
+// 匯出路由器
+module.exports = router
