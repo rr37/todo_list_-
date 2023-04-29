64cc9dc5bcc50035ef008a46166bd1695ff3e756
rr37 <ay870527@gmail.com>
2023-04-29 22:18:26 +0800

    modify todoSeeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,14 +1,36 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config
+}
 // 載入 todo model
 const Todo = require('../todo')
+const User = require('../user')
 const db = require('../../config/mongoose')
-
-
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
 
 // 連線成功
 db.once('open', () => {
-  for (let i =0; i < 10; i++) {
-    Todo.create({name:`name-${i}`})
-  }
-  
-  console.log('done')
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: 10 },
+        (_, i) => Todo.create({ name: `name-${i}`, userId })
+      ))
+    })
+    .then(() => {
+      console.log('seeder done.')
+      process.exit()
+    })
 })
